f165d09ef3fe0cf8ebfddfed33484de584401964
html <newparas11422@gmail.com>
2022-04-19 11:23:17 +0530

    generate-para

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from 'react';
+import data from './data';
 function App() {
+  let indexNo = 0
+  const [count, setCount] = useState(indexNo)
+  const [text, setText] = useState([])
+
+  const handleSubmit = (e)=>{
+    e.preventDefault()
+    let amount = parseInt(count)
+    if(count === 0){
+      // amount = 1
+     alert('Cant generate. So please enter a value')
+     }
+    setText(data.slice(0,amount)) 
+    setCount(0)
+   
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <section className="section-center">
+    <h3>generate 1-12 paragraph</h3>
+    <form onSubmit={handleSubmit} className="lorem-form">
+      <label htmlFor="amount">paragraph:</label>
+      <input type="number" name="amount" id="amount" value={count} onChange={(e)=> setCount(e.target.value)} min={(data.length - 1)-(data.length - 1)} max={data.length - 1}  />
+      <button type="submit" className='btn'>generate</button>
+      
+    </form>
+
+    <article className='lorem-text'>
+      {/* <ol><li><p></p> {data[0]}</li></ol> */}
+      <button className='btn' onClick={()=> setText([]) }>clear All</button>
+      {text.map((e,index)=>{
+        return(
+           <ul key={index}>
+             <li><p> {index+1}. {e}</p></li>
+           </ul>
+        )
+      })}
+     
+    </article>
+    {/* {console.log(text)} */}
+  </section>
+    )
 }
 
 export default App;
+ 
